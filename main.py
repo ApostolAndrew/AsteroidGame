9e9f17cb5dd79655576272d4ad989444be00eb79
ApostolAndrew <ApostAndrew@gmail.com>
2025-01-05 11:30:21 -0800

    add player groupings

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    #groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     #clock
     dt = 0
     clock = pygame.time.Clock()
 
     #player
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     #game loop
@@ -23,8 +28,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
