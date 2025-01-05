74511f14e1c2f4ab96e399dcacd26d87d0e433cf
ApostolAndrew <ApostAndrew@gmail.com>
2025-01-05 10:46:41 -0800

    added a 60 hz tick rate

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    dt = 0
+    clock = pygame.time.Clock()
 
     #game loop
     while True:
@@ -16,6 +18,7 @@ def main():
                 return
         pygame.Surface.fill(screen,(0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
